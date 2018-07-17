5dd978a93c87401d83316721d7e4288fa1f3421a
Pawan.singh <pawan.singh@digitaslbi.com>
2018-07-17 14:53:59 +0530

    init commit

---
+++ Alloy.Web/Models/Pages/FruitPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Alloy.Web.Models.Blocks;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace Alloy.Web.Models.Pages
+{
+
+    [SiteContentType(
+        GUID = "4A3F39A9-EB27-4B84-8166-A97F650A3781",
+        DisplayName = "My Fruit Page",
+        GroupName = Global.GroupNames.Fruit)]
+    [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-product.png")]
+    [AvailableContentTypes(
+        Availability = Availability.Specific,
+        IncludeOn = new[] { typeof(StartPage) })]
+    public class FruitPage : StandardPage, IHasRelatedContent
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 330)]
+        [CultureSpecific]
+        [AllowedTypes(new[] { typeof(IContentData) }, new[] { typeof(JumbotronBlock) })]
+        public virtual ContentArea RelatedContentArea { get; set; }
+    }
+}
